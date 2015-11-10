6064499e0e3fe5cc6aa12488f1fc05edaedb6141
zmitzie <accounts@mitzelos.com>
2015-11-10 21:50:45 +0000

    Repo init with mailchimp api

---
+++ app/controllers/newsletters_controller.rb
@@ -0,0 +1,29 @@
+class NewslettersController < ApplicationController
+  before_action :set_newsletter, only: [:show, :edit, :update, :destroy]
+  
+  def new
+    @newsletter = Newsletter.new
+  end
+  
+  def create
+    @newsletter = Newsletter.new(newsletter_params)
+    respond_to do |format|
+      if @newsletter.save
+        format.html { redirect_to root_path, notice: 'Successfully subscribed!' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_newsletter
+      @newsletter = Newsletter.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def newsletter_params
+      params.require(:newsletter).permit(:email)
+    end
+end
---
+++ app/models/newsletter.rb
@@ -0,0 +1,10 @@
+class Newsletter < ActiveRecord::Base
+  before_save :mailchimp
+  
+  protected
+    
+    def mailchimp
+      mailchimp = Mailchimp::API.new("6c1991e9312556e0235c650b9e4ef48c-us12")
+      mailchimp.lists.subscribe("4de1b79f01", { "email" => self.email })
+    end
+end
