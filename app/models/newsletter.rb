c5e782df66266bca6826eb3d23be898ed2e1feea
zmitzie <accounts@mitzelos.com>
2015-11-11 18:15:01 +0000

    Added validations for name and email

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,6 +1,14 @@
 class Newsletter < ActiveRecord::Base
   before_save :mailchimp
-  
+  validates :first_name, presence: true, length: { minimum: 3, maximum: 32 }
+  validates :last_name, presence: true, length: { minimum: 3, maximum: 32 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, 
+    presence: true,
+    length: {minimum: 3, maximum: 255},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+    
   protected
     
     def mailchimp
